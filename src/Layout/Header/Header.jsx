ee66b00762f36396c101f17cac51e6932ef81c12
ismail <ismailkoormath037@gmail.com>
2024-01-30 11:54:39 +0530

    header and hero sections ui development completed

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import logo from '../../assest/Aloha.svg'
+import menu from '../../assest/menu_Icon.svg'
+import classes from './Header.module.css'
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className={classes.Header}>
+            <div className={classes.header_left}>
+                <img src={logo} alt="logo" />
+                <ul>
+                    <li>Home</li>
+                    <li>Surfing</li>
+                    <li>Hula</li>
+                    <li>Vulcano</li>
+                </ul>
+            </div>
+            <button className={classes.booking_btn}>Book a trip</button>
+            <img className={classes.menu} src={menu} alt="menu" />
+        </div>
+    )
 }
 
 export default Header
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Header from '../../Layout/Header/Header'
+import classes from './Home.module.css'
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className={classes.home_page}>
+            <section className={classes.hero_section}>
+                <Header />
+                <h1 className={classes.hero_Text}>Welcome<br />
+                    to Hawaii</h1>
+            </section>
+
+        </div>
+    )
 }
 
 export default Home
