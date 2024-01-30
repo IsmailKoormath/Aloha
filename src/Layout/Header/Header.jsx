2d32431d4dd3e908538089782a0f5d0d6f094e53
ismail <ismailkoormath037@gmail.com>
2024-01-30 12:56:04 +0530

    header responsive completed

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assest/Aloha.svg'
 import menu from '../../assest/menu_Icon.svg'
+import cross from '../../assest/close.svg'
 import classes from './Header.module.css'
 const Header = () => {
+    const [show, setShow] = useState(false)
     return (
         <div className={classes.Header}>
-            <div className={classes.header_left}>
-                <img src={logo} alt="logo" />
+            {/* <div className={classes.header_left}> */}
+            <img src={logo} alt="logo" />
+            <div className={`${classes.header_right} ${show && classes.menus}`}>
+                <img className={classes.crossIcon} onClick={() => setShow(false)} src={cross} alt='close' />
                 <ul>
                     <li>Home</li>
                     <li>Surfing</li>
                     <li>Hula</li>
                     <li>Vulcano</li>
                 </ul>
+                {/* </div> */}
+                <button className={classes.booking_btn}>Book a trip</button>
             </div>
-            <button className={classes.booking_btn}>Book a trip</button>
-            <img className={classes.menu} src={menu} alt="menu" />
+            <img onClick={() => setShow(true)} className={classes.menu} src={menu} alt="menu" />
         </div>
     )
 }
---
+++ src/Pages/Home/Component/Highlights/Highlights.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './Highlights.module.css'
+const Highlights = () => {
+  return (
+    <div className={classes.highlights}>
+          <h4 className={classes.heading}>Highlights</h4>
+    </div>
+  )
+}
+
+export default Highlights
